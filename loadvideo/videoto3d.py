1ba2053e0feecb93134aa6dc9c07dd788c3fef6e
ryslil <rysryuta@gmail.com>
2017-01-11 09:25:03 +0900

    Add a method

--- loadvideo/videoto3d.py
+++ loadvideo/videoto3d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 class Videoto3D:
 
     def __init__(self, width, height, depth):
@@ -11,18 +12,19 @@ class Videoto3D:
     def video3d(self, filename):
         cap = cv2.VideoCapture(filename)
         nframe = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        frames = [x*nframe/self.depth for x in range(self.depth)]
+        frames = [x * nframe / self.depth for x in range(self.depth)]
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        gray = []
 
         for i in range(self.depth):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frames[i])
             ret, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite('file{}.png'.format(i), gray)
+            gray.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+            #cv2.imwrite('file{}.png'.format(i), gray[0])
 
+        cap.release()
 
+    def get_UCF_classname(self, filename):
+        return filename[2:filename.find('_', 2)]
 
-if __name__ == '__main__':
-    vid = Videoto3D(120, 90, 10)
-    vid.video3d('v_SkateBoarding_g25_c02.avi')
