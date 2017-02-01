8189ca5305dc98f214c8cda74bcd7d954ca88287
rysmarie <rysryuta@gmail.com>
2017-02-01 17:31:37 +0900

    Add results

--- videoto3d.py
+++ videoto3d.py
@@ -13,13 +13,14 @@ class Videoto3D:
         cap = cv2.VideoCapture(filename)
         nframe = cap.get(cv2.CAP_PROP_FRAME_COUNT)
         frames = [x * nframe / self.depth for x in range(self.depth)]
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        #cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         gray = []
 
         for i in range(self.depth):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frames[i])
             ret, frame = cap.read()
+            frame = cv2.resize(frame, (self.height, self.width))
             gray.append(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
             #cv2.imwrite('file{}.png'.format(i), gray[0])
 
